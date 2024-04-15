c348c2cd4a1c0e44fa8098b77b519bc1921e9d89
yashpatel5400 <ypatel5400@gmail.com>
2024-04-15 18:05:43 -0400

    Minor chagne

--- run_exp.sh
+++ run_exp.sh
@@ -1,6 +1,3 @@
 python wno.py --pde diff-react --kernel 3
-python wno.py --pde diff-react --kernel 5
 python wno.py --pde Darcy --kernel 3
-python wno.py --pde Darcy --kernel 5
 python wno.py --pde rdb --kernel 3
-python wno.py --pde rdb --kernel 5
